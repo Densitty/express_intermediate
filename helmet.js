ba948a3a2622aee980181b02b1e145485222b961
Densitty <dennis.brough@yandex.com>
2021-02-20 15:16:33 -0600

    reading from the body and protecting the request headers using helmet

---
+++ helmet.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+
+// wear your helmet to protect your application request headers
+app.use(helmet());
+
+app.use(express.static("public"));
+// middleware in setting request headers
+app.use(express.json());
+// middleware for reading data from the request body
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/ajax", (req, res) => {
+  console.log(req.headers["content-type"]);
+  console.log(req.body);
+  res.send("POSTing");
+});
+
+app.listen(3000);
+console.log("App running on port 3000");
